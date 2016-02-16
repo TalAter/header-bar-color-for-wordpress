24f22d1dd23cb2076f74039331a8da89b3c82896
Tal Ater <tal@talater.com>
2016-02-16 14:23:32 +0200

    Added menu item, views and empty settings page

--- class.header-bar-color-wp-admin.php
+++ class.header-bar-color-wp-admin.php
@@ -16,5 +16,27 @@ class Header_Bar_Color_WP_Admin {
    * Registers hooks for admin actions
    */
   private static function init_hooks() {
+    add_action('admin_menu', array('Header_Bar_Color_WP_Admin', 'add_menu'));
   }
+
+  /**
+   * Adds the options menu item
+   */
+  public static function add_menu() {
+    add_submenu_page('themes.php', 'Header Bar Color', 'Header Bar Color', 'manage_options', 'hebacowp-settings', array('Header_Bar_Color_WP_Admin', 'admin_page_settings'));
+  }
+
+  public static function admin_page_settings() {
+    self::view('admin_page_settings');
+  }
+
+  /**
+   * Renders a view
+   *
+   * @param $view_name string Name of the view to render
+   */
+  private static function view($view_name) {
+    include(HEBACOWP_PLUGIN_DIR.'views'.DIRECTORY_SEPARATOR.$view_name.'.php');
+  }
+
 }
