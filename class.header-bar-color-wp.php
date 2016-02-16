d301a7d7e93af36dfaa1f498e4e38ec48733f2cc
Tal Ater <tal@talater.com>
2016-02-16 15:35:51 +0200

    Added header output in <head>

--- class.header-bar-color-wp.php
+++ class.header-bar-color-wp.php
@@ -18,6 +18,28 @@ class Header_Bar_Color_WP {
    * Registers hooks for plugin actions
    */
   private static function init_hooks() {
+    add_action('wp_head', array('Header_Bar_Color_WP', 'add_headers'));
+  }
+
+  private static function getHeaderColor() {
+    $color = get_option('hebacowp-color');
+    if (empty($color)) {
+      $color = Header_Bar_Color_WP::$defaultColor;
+    }
+    return $color;
+  }
+
+  /**
+   *
+   */
+  public static function add_headers() {
+    $color = self::getHeaderColor();
+    echo
+      PHP_EOL.
+      '<!-- Header Bar Color for WordPress Plugin -->'.PHP_EOL.
+      '<meta name="theme-color" content="'.$color.'">'.PHP_EOL.
+      '<meta name="msapplication-navbutton-color" content="'.$color.'">'.PHP_EOL.
+      '<meta name="apple-mobile-web-app-status-bar-style" content="'.$color.'">'.PHP_EOL;
   }
 
 }
